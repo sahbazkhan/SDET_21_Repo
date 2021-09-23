cfda8439efc2c92f68d30b43ecb09806efe685ff
sahbazkhan <sahbazkhan31@gmail.com>
2021-09-23 02:23:32 -0700

    hi push the code

--- ComcastSeleniumFirm/src/test/java/com/casttest/ContactTest.java
+++ ComcastSeleniumFirm/src/test/java/com/casttest/ContactTest.java
@@ -6,6 +6,10 @@ public class ContactTest {
 	@Test(groups="smoke")
 	public void crearecontact() {
 		System.out.println("execute create");
+		String BROWSER=System.getProperty("browser");
+		System.out.println(BROWSER);
+		String ENV=System.getProperty("url");
+		System.out.println(ENV);
 	}
        @Test(groups="regionl")
        public void createwithcontact() {
