ff493f7947dd4806bd66d2efed01a6887cd710ca
sahbazkhan <sahbazkhan31@gmail.com>
2021-09-22 22:52:09 -0700

    push the code

---
+++ ComcastSeleniumFirm/src/test/java/com/casttest/ContactTest.java
@@ -0,0 +1,19 @@
+package com.casttest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test(groups="smoke")
+	public void crearecontact() {
+		System.out.println("execute create");
+	}
+       @Test(groups="regionl")
+       public void createwithcontact() {
+    	   System.out.println("execute create with");
+    	   
+       }
+       @Test(groups="regionl")
+       public void deletecontact() {
+    	   System.out.println("deletecontact");
+       }
+}
---
+++ ComcastSeleniumFirm/src/test/java/com/comcast/org/EditcontactTest.java
@@ -0,0 +1,15 @@
+package com.comcast.org;
+
+import org.testng.annotations.Test;
+
+public class EditcontactTest {
+	@Test(groups="regionl")
+	public void editcontactTest() {
+		System.out.println("execute delete");
+	}
+	@Test(groups="smoke")
+	public void editcontactcreateTest() {
+		System.out.println("executeedit");
+	}
+
+}
